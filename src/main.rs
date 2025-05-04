0bd5e239a104f05c2a6ab4634edc885837ef0f67
Galadima3 <abrahamgaladima300@gmail.com>
2025-05-04 15:20:05 +0100

    2nd Commit

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,49 @@
+use std::io;
+
 fn main() {
+    /*
     // Constants
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
-    // floating point
-    let height: f32 = 4.50;
+     // floating point
+     let height: f32 = 4.50;
 
-    // bool
-    let is_nysc_fun: bool = false;
+     // bool
+     let is_nysc_fun: bool = false;
+    */
 
+    // Tuple
     let employee: (&str, &str, i8) = ("James", "Software Developer", 29);
     let (_name, _designation, _age) = employee;
-
     let worker_name: &str = employee.0;
+    println!("Employee name is: {worker_name}");
 
-    println!("Employee name is: {worker_name}"); 
+    // Arrays
+    let colors: [&str; 5] = ["red", "purple", "yellow", "blue", "green"];
 
+    loop {
+        println!("Enter enter an array index ...");
+        let mut index = String::new();
 
+        io::stdin()
+            .read_line(&mut index)
+            .expect("Failed to get input... ");
+
+        let index: usize = match index.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a valid number!");
+                continue;
+            }
+        };
+
+        if index >= colors.len() {
+            println!("Index out of bounds! Max index is {}", colors.len() - 1);
+            continue;
+        } else {
+            let element = colors[index];
+            println!("The value of the element at index {index} is: {element}");
+            break;
+        }
+    }
 }
